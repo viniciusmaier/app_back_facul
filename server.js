65e51659ae3241d838cd4751f621a10f937712e5
vinicius maier <viniciusfiel1000@gmail.com>
2024-03-05 23:27:09 -0300

    adicionando os arquivos do heroku

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
 const Express = require("express");
+const port = process.env.PORT || 4000;
 const routerAluno = require("./router/routerAluno");
 const routerProfessor = require("./router/routerProfessor");
 const app = Express();
+
 app.use(Express.json());
 app.use("/aluno", routerAluno);
 app.use("/professor", routerProfessor);
 require("./models/association/association-aluno-curso");
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log("logado");
 });
