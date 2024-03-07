431fbadbd6a143da5e27247a35f52e22da3eb789
vinicius maier <viniciusfiel1000@gmail.com>
2024-03-07 01:03:56 -0300

    migração para o Render

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database_url: process.env.DB_DATABASE_URL,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   },
--- config/database/sequelize.js
+++ config/database/sequelize.js
@@ -1,19 +1,28 @@
 const { Sequelize } = require("sequelize");
 const config = require("../config").development;
-
 try {
-  const conexao = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    {
-      dialect: config.dialect,
-      host: config.host,
-      define: {
-        caseSensitive: false,
+  const conexao = new Sequelize(config.database_url, {
+    dialect: config.dialect,
+    dialectOptions: {
+      connectTimeout: 3000,
+      ssl: {
+        require: "true",
       },
-    }
-  );
+    },
+  });
+
+  // const conexao = new Sequelize(
+  //   config.database,
+  //   config.username,
+  //   config.password,
+  //   {
+  //     dialect: config.dialect,
+  //     host: config.host,
+  //     define: {
+  //       caseSensitive: false,
+  //     },
+  //   }
+  // );
   module.exports = conexao;
 } catch (err) {
   console.log("deu erro");
