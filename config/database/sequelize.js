91f0a9be9eb058e58a59946001f446854620cec3
vinicius maier <viniciusfiel1000@gmail.com>
2024-03-08 00:12:55 -0300

    teste render

--- config/database/sequelize.js
+++ config/database/sequelize.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require("sequelize");
 const config = require("../config").development;
 try {
-  const conexao = new Sequelize(config.database_url, {
-    dialect: config.dialect,
+  const conexao = new Sequelize(process.env.DATA_BASE_URL, {
+    dialect: "postgres",
     dialectOptions: {
       connectTimeout: 3000,
       ssl: {
@@ -11,18 +11,6 @@ try {
     },
   });
 
-  // const conexao = new Sequelize(
-  //   config.database,
-  //   config.username,
-  //   config.password,
-  //   {
-  //     dialect: config.dialect,
-  //     host: config.host,
-  //     define: {
-  //       caseSensitive: false,
-  //     },
-  //   }
-  // );
   module.exports = conexao;
 } catch (err) {
   console.log("deu erro");
