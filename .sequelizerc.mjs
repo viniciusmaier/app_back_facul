ea6034613528cae70111fb66512d3e68719c25ec
vinicius maier <viniciusfiel1000@gmail.com>
2024-03-26 00:35:20 -0300

    teste mudando extensao

--- .sequelizerc.mjs
+++ .sequelizerc.mjs
@@ -1,7 +1,7 @@
 import path from "path";
 
 module.exports = {
-  config: path.resolve("../app_back_facul/config/"),
+  config: path.resolve("../app_back_facul/config/config.js"),
   "models-path": path.resolve("../app_back_facul", "/src/models"),
   "migrations-path": path.resolve("../app_back_facul", "/migrations"),
 };
