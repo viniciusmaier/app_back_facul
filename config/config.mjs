ece3a79839b4c039198649d8d86b9416e24cf8b1
Vinicius Maier Lucio <vinicius.lucio@oderco.local>
2024-03-26 09:34:33 -0300

    Atualizando config #01

--- config/config.mjs
+++ config/config.mjs
@@ -1,29 +1,20 @@
-import dotenv from "dotenv";
+
 import fs from "fs";
-dotenv.config();
+const configJson = "./config/config.json";
 
-const configJson = "./config.mjs";
-
-fs.readFile(configJson, "utf8", (error, data) => {
-  if (error) {
-    console.error("Erro ao ler o arquivo config.json:", err);
-    return;
+const configConection = [{
+  "development": {
+    "username": `${process.env.DB_USER}`,
+    "password": `${process.env.DB_PASSWORD}`,
+    "database": `${process.env.DB_DATABASE}`,
+    "host": `${process.env.DB_HOST}`,
+    "dialect": `${process.env.DIALECT}`
   }
+}
+]
 
-  let config = JSON.parse(data);
-  config.development.username = process.env.DB_USER;
-  config.development.password = process.env.DB_PASSWORD;
-  config.development.database = process.env.DB_DATABASE;
-  config.development.host = process.env.DB_HOST;
-  config.development.dialect = process.env.DIALECT;
 
-  const updateConfig = JSON.stringify(config, null, 2);
+fs.writeFileSync(configJson, JSON.stringify(configConection[0]), { encoding: "utf8" });
 
-  fs.writeFile(configJson, updateConfig, "utf8", (err) => {
-    if (err) {
-      console.error(`ERROR :  ${err}`);
-      return;
-    }
-    console.log("STATUS : CONFIGURACOES ATUALIZADAS");
-  });
-});
+
+
