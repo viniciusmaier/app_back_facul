374512d162412263fbc89eed9e61a6ebe7f68524
vinicius maier <viniciusfiel1000@gmail.com>
2024-03-26 00:01:53 -0300

    ajustando as configuracoes do sequelize

--- .sequelizerc.mjs
+++ .sequelizerc.mjs
@@ -1,7 +1,7 @@
-    import path from 'path';
+import path from "path";
 
-    export const SequelizeConfig = {
-        'config' : path.resolve('config/config.mjs'),
-        'models-path' : path.resolve('src/models'),
-        'migrations-path' : path.resolve('migrations')
-    }
+export const SequelizeConfig = {
+  config: path.resolve("../app_back_facul/config", "/config.mjs"),
+  "models-path": path.resolve("../app_back_facul", "/src/models"),
+  "migrations-path": path.resolve("../app_back_facul", "/migrations"),
+};
