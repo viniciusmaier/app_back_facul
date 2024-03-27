06ba1594e956935365e36d0cd071bd6e4bed5512
vinicius maier <viniciusfiel1000@gmail.com>
2024-03-26 21:01:51 -0300

    adicionando insercao de dados em massa

--- src/service/serviceAluno.mjs
+++ src/service/serviceAluno.mjs
@@ -14,22 +14,44 @@ export default class serviceAluno {
     if (!dadosAluno) {
       return;
     }
-    const cryptoPassword = await bcrypt.hash(dadosAluno.password, 10);
-    try {
-      const resp = PessoaModel.cadastrarUsuario({
-        nome: dadosAluno.nome,
-        ra: dadosAluno.ra,
-        email: dadosAluno.email,
-        password: cryptoPassword,
-        telefone1: dadosAluno.telefone1,
-        telefone2: dadosAluno.telefone2,
-        bo_aluno: true,
-        bo_professor: false,
-        bo_coordenacao: false,
-      }).then((resultado) => console.log(resultado));
-    } catch (err) {
-      console.log(err);
+
+    if (dadosAluno.length <= 1) {
+      const cryptoPassword = await bcrypt.hash(dadosAluno.password, 10);
+      try {
+        const resp = PessoaModel.cadastrarUsuario({
+          nome: dadosAluno.nome,
+          ra: dadosAluno.ra,
+          email: dadosAluno.email,
+          password: cryptoPassword,
+          telefone1: dadosAluno.telefone1,
+          telefone2: dadosAluno.telefone2,
+          bo_aluno: true,
+          bo_professor: false,
+          bo_coordenacao: false,
+        }).then((resultado) => console.log(resultado));
+      } catch (err) {
+        console.log(err);
+      }
     }
+
+    dadosAluno.map(async (dado) => {
+      const cryptoPassword = await bcrypt.hash(dadosAluno.password, 10);
+      try {
+        const resp = PessoaModel.cadastrarUsuario({
+          nome: dadosAluno.nome,
+          ra: dadosAluno.ra,
+          email: dadosAluno.email,
+          password: cryptoPassword,
+          telefone1: dadosAluno.telefone1,
+          telefone2: dadosAluno.telefone2,
+          bo_aluno: true,
+          bo_professor: false,
+          bo_coordenacao: false,
+        }).then((resultado) => console.log(resultado));
+      } catch (err) {
+        console.log(err);
+      }
+    });
   }
 
   async loginPessoa(dadosLogin) {
